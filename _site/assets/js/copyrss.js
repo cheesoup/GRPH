f6a27ec0180e0ef01f8293409093664de4dc53a9
cheesoup <55515711+cheesoup@users.noreply.github.com>
2021-11-15 21:48:43 -0500

    Fancy buttonz!

--- _site/assets/js/copyrss.js
+++
@@ -1,9 +0,0 @@
-function copyURI(evt) {
-    evt.preventDefault();
-    navigator.clipboard.writeText(evt.target.getAttribute('data-url')).then(() => {
-      document.getElementById("copied").style.opacity = "1";
-      setTimeout(() => {  document.getElementById("copied").style.opacity = "0"; }, 2000);
-    }, () => {
-      /* clipboard write failed */
-    });
-}
